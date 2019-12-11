2396f6e91dec7238a4a6639dd9c782ef0a73356e
unknown <hector.vicencio.reyes@gmail.com>
2019-12-11 00:44:13 -0300

    adding calendar and chart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './App.css';
+import DatePicker from './components/DatePicker';
+import { Grid, Row, Col} from "react-flexbox-grid";
+import Chart from './components/Chart';
 
 function App() {
   return (
     <div className="App">
-      hola
+      <Grid>
+        <Row>
+          <Col xs={12} md={6}>  
+            <DatePicker data={"hola"}/>
+          </Col>
+          <Col xs={12} md={6}>
+            <DatePicker data={"adios"}/>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs={12}>
+            <Chart/>
+          </Col>
+        </Row>
+        
+      </Grid>
     </div>
   );
 }
